9976ddea188b4576651862dbe89a0b46e2393f4d
Flavio Faria <135338397+flavifaria@users.noreply.github.com>
2025-03-24 08:47:21 -0300

    Atualização do código em python

--- python.py
+++ python.py
@@ -1,1 +1,8 @@
-nome = input("Qual eu nome ?")
+idade = int(input('Qual sua idade ? '))
+
+if idade < 16:
+    print("Não precisa votar!")
+elif idade == 16 or idade > 65:
+    print("Voto não obrigatório")
+else:
+    print("Voto obrigatório")
