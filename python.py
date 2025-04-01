afc48999036b670f5bff199a992813b633b9afc9
Flavio Faria <135338397+flavifaria@users.noreply.github.com>
2025-04-01 11:57:08 -0300

    Delete python.py

--- python.py
+++
@@ -1,8 +0,0 @@
-idade = int(input('Qual sua idade ? '))
-
-if idade < 16:
-    print("Não precisa votar!")
-elif idade == 16 or idade > 65:
-    print("Voto não obrigatório")
-else:
-    print("Voto obrigatório")
